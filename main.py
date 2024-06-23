b96293695218eeeca0379b2aed7525da0cf69fed
TrinidadAzpiri <102036228+TrinidadAzpiri@users.noreply.github.com>
2024-06-23 17:23:40 -0300

    main.py streamlit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import pip
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
 import pickle
+from streamlit_option_menu import option_menu
+
 
 
 # from pyspark.sql.functions import col, explode
@@ -22,4 +25,26 @@ data = pd.read_csv("data\df_limpio.csv")
 
 #2. Titulo de pagina
 st.header("Sistema de recomendación de restaurantes")
+
+#3. Sidebar
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title='Menu',
+        options=['Home', 'Visualizando los datos', 'Armado del modelo', 'Encontrá tu restaurant'],
+    )
+
+# Resto del código
+if selected == 'Home':
+    st.title("Home")
+    st.write("Bienvenido a la página principal.")
+elif selected == 'Visualizando los datos':
+    st.title("Visualizando los datos")
+    st.write("Aquí puedes visualizar los datos.")
+elif selected == 'Armado del modelo':
+    st.title("Armado del modelo")
+    st.write("Aquí puedes armar el modelo.")
+elif selected == 'Encontrá tu libro':
+    st.title("Encontrá tu libro")
+    st.write("Aquí puedes encontrar tu libro.")
 
